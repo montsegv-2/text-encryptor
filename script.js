abc59f1ff0ad761599127f196b392a38a7aec3e6
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-03-04 15:01:19 -0600

    Add text encryption

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar() {
+  const textoEncriptado = encriptar(textArea.value);
+  mensaje.value = textoEncriptado;
+}
+
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.include(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
+    }
+  }
+
+  return stringEncriptada;
+}
